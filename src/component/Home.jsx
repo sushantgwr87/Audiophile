e1a1794f37a573be69190a34a131d95f9cbff030
sushantgwr87 <sushantgangwar87@gmail.com>
2022-04-02 10:38:46 +0530

    Added Home Component

---
+++ src/component/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid rem ex dolore vero adipisci ipsa, quas soluta doloribus! Repudiandae, provident alias. Atque sunt ad distinctio enim, porro alias ab eveniet.</div>
+  )
+}
+
+export default Home
