3da17383d3bac4421b5a3b495e1f1c112e8281a8
sushantgwr87 <sushantgangwar87@gmail.com>
2022-04-03 01:35:03 +0530

    Updated Navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import useBreakpoints from '../customHook/useBreakpoints';
 import styles from '../styles/navbar.module.css';
+import Icon from '../Icon';
 // import useMountTransition from '../customHook/useMountTransition';
 
 const Navbar = () => {
@@ -44,53 +45,60 @@ const Navbar = () => {
                     (
                         <>
                             <Link to="/">
-                                Logo
+                                <Icon name="AudioName" width={150} fill="white" />
                             </Link>
                             <div className={styles.nav_desktop}>
                                 <ul>
-                                    <Link to="/stories">Stories</Link>
-                                    <Link to="/features">Features</Link>
-                                    <Link to="/pricing">Pricing</Link>
+                                    <Link to="/stories">Home</Link>
+                                    <Link to="/features">Headphones</Link>
+                                    <Link to="/pricing">Speakers</Link>
+                                    <Link to="/pricing">Earphones</Link>
                                 </ul>
-                                <button className={styles.nav_modal___btn}>Get an invite</button>
+                                {/* <button className={styles.nav_modal___btn}>Get an invite</button> */}
+                                <Icon name="Cart" width={40} />
                             </div>
                         </>
                     )
                     :
                     <>
-                                        <div ref={sidebar}>
-                        <div className={showSidebar ? styles.nav_mobile_menu___btn_close : styles.nav_mobile_menu___btn_open} onClick={() => setShowSidebar(!showSidebar)}></div>
-                        <div className={`${styles.nav_mobile} ${showSidebar ? styles.nav_mobile___show : styles.nav_mobile___hide}`}>
-                            <ul>
-                                <li>
-                                    <Link onClick={closeNav} to="/stories">
-                                        Stories
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link onClick={closeNav} to="/features">
-                                        Features
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link onClick={closeNav} to="/pricing">
-                                        Home
-                                    </Link>
-                                </li>
-                                <li>
-                                    <button className={styles.nav_modal___btn}>Get an invite</button>
-                                </li>
-                            </ul>
+                        <div ref={sidebar}>
+                            <div className={showSidebar ? styles.nav_mobile_menu___btn_close : styles.nav_mobile_menu___btn_open} onClick={() => setShowSidebar(!showSidebar)}></div>
+                            <div className={`${styles.nav_mobile} ${showSidebar ? styles.nav_mobile___show : styles.nav_mobile___hide}`}>
+                                <ul>
+                                    <li>
+                                        <Link onClick={closeNav} to="/stories">
+                                            Home
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link onClick={closeNav} to="/features">
+                                            Headphones
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link onClick={closeNav} to="/pricing">
+                                            Speakers
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link onClick={closeNav} to="/pricing">
+                                            Earphones
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <button className={styles.nav_modal___btn}>Get an invite</button>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                    <Link to="/">
+                        <div>
+                            <Link to="/">
                                 Logo
                             </Link>
-                    </div>
-                    <div>
-                        cart
-                    </div>
+                        </div>
+                        <div>
+                            cart
+                        </div>
                     </>
                 }
             </nav>
