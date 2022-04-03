e8d952d418027fb1f4111607328c4098cbb89db3
sushantgwr87 <sushantgangwar87@gmail.com>
2022-04-03 05:34:25 +0530

    Updated Breakpoint Hook

--- src/customHook/useBreakpoints.js
+++ src/customHook/useBreakpoints.js
@@ -6,7 +6,7 @@ export default function useBreakpoints() {
     const breakpoints = {
         isXs: useMediaQuery("(max-width: 640px)"),
         isSm: useMediaQuery("(min-width: 641px) and (max-width: 768px)"),
-        isMd: useMediaQuery("(min-width: 769px)"),
+        isMd: useMediaQuery("(min-width: 900px)"),
     };
 
     return breakpoints;
