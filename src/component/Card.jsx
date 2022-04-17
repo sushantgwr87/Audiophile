a014beb02bc7bd92c84a8b465ef47e485642c29c
sushantgwr87 <sushantgangwar87@gmail.com>
2022-04-17 06:29:10 +0530

    Updated Card Component

---
+++ src/component/Card.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Icon from '../Icon';
+import styles from '../styles/card.module.css';
+// import Modal from './Modal';
+import { Link } from 'react-router-dom';
+
+const Card = ({ imagePath, cardHead, cardBody, isModal = false, isbutton = true, isdark = true, isreverse = false, buttonText }) => {
+
+    const [modalShow, setModalShow] = React.useState(false);
+
+    return (
+        <div className={`${styles.card} ${isdark ? styles.card___dark : styles.card___light} ${isreverse && styles.card___reverse}`}>
+            <div className={styles.card_photo}>
+                <img src={imagePath} alt='CardImage' />
+            </div>
+            <div className={styles.card_content}>
+                <h3>{cardHead}</h3>
+                <p>{cardBody}</p>
+                {isbutton && (isModal ? (
+                    <button className='card_btn___hover' onClick={() => setModalShow(true)}>
+                        <span>{buttonText}</span>
+                        <div className="arrow">
+                            <Icon name="arrow" fill={isdark ? "#fff" : "#000"} />
+                        </div>
+                    </button>
+                ) :
+                    <Link to="/">
+                            <button className='card_btn___hover'>
+                                <span>{buttonText}</span>
+                                <div className="arrow">
+                                    <Icon name="arrow" fill={isdark ? "#fff" : "#000"} />
+                                </div>
+                            </button>
+                    </Link>
+                )
+                }
+                {/* <Modal onClose={() => setModalShow(false)} show={modalShow} /> */}
+            </div>
+        </div>
+    );
+};
+
+export default Card;
