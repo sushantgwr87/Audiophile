063235c1d5473e7136065115f3584e30302f8093
sushantgwr87 <sushantgangwar87@gmail.com>
2022-04-17 06:29:22 +0530

    Updated Home Component

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React from 'react';
+import Card from './Card';
+
+const introductionData = [
+  {
+    path: '/assets/headphone_crop2.jpg',
+    head: 'create and share your photo stories.',
+    body: 'Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.',
+    buttonText: 'Get an invite'
+  },
+  {
+    path: '',
+    head: 'Beautiful stories every time',
+    body: 'We provide design templates to ensure your stories look terrific. Easily add photos, text and embed media from other networks, Then share your story with everyone',
+    buttonText: 'View the Stories'
+  },
+  {
+    path: '',
+    head: 'Designed for Everyone',
+    body: 'Photosnap can help you create stories that resonates with your audience. Our tool is designed for photographers of all levels, brands, businesses you name it.',
+    buttonText: 'View the Stories'
+  },
+]
 
 const Home = () => {
   return (
-    <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid rem ex dolore vero adipisci ipsa, quas soluta doloribus! Repudiandae, provident alias. Atque sunt ad distinctio enim, porro alias ab eveniet.</div>
+    introductionData.map((value, index) => (
+      <Card
+        imagePath={value.path}
+        cardHead={value.head}
+        cardBody={value.body}
+        isdark={index > 0 ? false : true}
+        isModal={index===0 && true}
+        isreverse={index === 1 && true}
+        buttonText={value.buttonText}
+        key={index}
+      />
+    ))
   )
 }
 
