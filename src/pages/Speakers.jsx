4b7ebf82b09035a7132e2e2ece504526317d1cd6
sushantgwr87 <sushantgangwar87@gmail.com>
2022-04-19 15:49:20 +0530

    Added Speakers Route Component

---
+++ src/pages/Speakers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Speakers = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus sed cupiditate non obcaecati expedita repellat. Modi tempora, suscipit alias harum odit tenetur repudiandae nisi dolor praesentium, minima ipsum nesciunt quisquam!</div>
+  )
+}
+
+export default Speakers
