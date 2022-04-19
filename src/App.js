35a679541b96a1908ae3e0cdeb8e5dd202db4feb
sushantgwr87 <sushantgangwar87@gmail.com>
2022-04-19 15:47:33 +0530

    Updated Routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import Navbar from "./component/Navbar";
-import Home from "./component/Home";
+import Footer from "./component/Footer";
+import Headphones from "./pages/Headphones";
+import Earphones from "./pages/Earphones";
+import Speakers from "./pages/Speakers";
+import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <div className="App">
+      <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/headphones" element={<Headphones />} />
+          <Route path="/speakers" element={<Speakers />} />
+          <Route path="/earphones" element={<Earphones />} />
+
           {/* <Route component={NotFound} /> */}
         </Routes>
       </div>
+      <Footer />
     </Router>
   );
 }
