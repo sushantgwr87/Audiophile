a8b5afd01de4fc4ff000a20df8529c45f22af42a
sushantgwr87 <sushantgangwar87@gmail.com>
2022-04-19 15:48:33 +0530

    Updated Icon Index

--- src/Icon/index.js
+++ src/Icon/index.js
@@ -1,6 +1,9 @@
 import AudioLogo from "./AudiophileLogo";
 import AudioName from "./AudiophileName";
 import Cart from "./Cart";
+import Github from "./GithubLogo";
+import Linkedin from "./LinkedinLogo";
+import Portfolio from "./PortfolioLogo";
 
 const Icon = props => {
   switch (props.name) {
@@ -10,6 +13,12 @@ const Icon = props => {
       return <AudioName {...props} />;
     case "Cart":
       return <Cart {...props} />;
+    case "github":
+      return <Github {...props} />;
+    case "linkedin":
+      return <Linkedin {...props} />;
+    case "portfolio":
+      return <Portfolio {...props} />;
     default:
       return;
   }
