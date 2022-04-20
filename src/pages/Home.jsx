03a305cccf46d9df0aef9d9a37b6c3521a0752b3
sushantgwr87 <sushantgangwar87@gmail.com>
2022-04-21 00:29:40 +0530

    Updated Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Card from '../component/Card';
 
-const introductionData = [
-  {
-    path: '/assets/headphone_wearing_person.jpg',
-    head: 'Bringing you the best audio gear',
-    body: 'Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.',
-  },
-]
+const featuredCardData =
+{
+  path: '/assets/headphone_wearing_person.jpg',
+  head: 'Bringing you the best audio gear',
+  body: 'Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.',
+}
 
 const Home = () => {
   return (
@@ -26,19 +25,14 @@ const Home = () => {
         </div>
       </div>
       <div>
-      {introductionData.map((value, index) =>
         <Card
-          cardQuote={value.quote}
-          imagePath={value.path}
-          cardHead={value.head}
-          cardBody={value.body}
-          isdark={index === 0 ? false : true}
-          isModal={index === 0 && false}
-          isbutton={index === 0 && false}
-          buttonText={value.buttonText}
-          key={index}
+          cardQuote={featuredCardData.quote}
+          imagePath={featuredCardData.path}
+          cardHead={featuredCardData.head}
+          cardBody={featuredCardData.body}
+          buttonText={featuredCardData.buttonText}
+          isbutton={false}
         />
-      )}
       </div>
     </>
   )
