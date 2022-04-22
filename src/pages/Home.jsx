a61e1821830d3ff01e2909ff1c9a4fb6d6260caa
sushantgwr87 <sushantgangwar87@gmail.com>
2022-04-22 06:46:40 +0530

    Updated Home Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,10 @@ import Card from '../component/Card';
 
 const productCardData =
 {
-  path: '/assets/headphone_side.png',
+  path: '/assets/headphone_white2.png',
   head: 'Bringing you the best audio gear',
   body: 'Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.',
+  buttonText : 'See product'
 }
 
 const Home = () => {
