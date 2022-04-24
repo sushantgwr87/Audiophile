0ebe976462e9063a39aa2376a95f37d8a73b30b1
sushantgwr87 <sushantgangwar87@gmail.com>
2022-04-24 13:41:43 +0530

    Updated Speakers Page

--- src/pages/Speakers.jsx
+++ src/pages/Speakers.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React from 'react';
+import Card from '../component/Card';
+
+const productData = [
+  {
+    path: "/assets/speaker_flat.png",
+    quote: "New Product",
+    head: "XX99 Mark II Headphones",
+    body: "Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.",
+    buttonText: "See Product"
+  },
+  {
+    path: "/assets/speaker.png",
+    head: "XX99 Mark I Headphones",
+    body: "Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.",
+    buttonText: "See Product"
+  },
+  {
+    path: "/assets/speaker_top2.png",
+    head: "X10 Mark Headphones",
+    body: "Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.",
+    buttonText: "See Product"
+  },
+]
 
 const Speakers = () => {
   return (
-    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus sed cupiditate non obcaecati expedita repellat. Modi tempora, suscipit alias harum odit tenetur repudiandae nisi dolor praesentium, minima ipsum nesciunt quisquam!</div>
+    <>
+      <div className="page_title">
+        Speakers
+      </div>
+      <div className="page_content">
+        {productData.map((value, index) =>
+          <Card
+            key={index}
+            imagePath={value.path}
+            cardQuote={value.quote}
+            cardBody={value.body}
+            cardHead={value.head}
+            buttonText={value.buttonText}
+            isbutton={true}
+            isreverse={index === 1 && true}
+          />
+        )}
+      </div>
+    </>
   )
 }
 
