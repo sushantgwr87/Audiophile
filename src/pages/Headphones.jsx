55f9c69bd503a12fd4b55508cd1b52909a6dcbb8
sushantgwr87 <sushantgangwar87@gmail.com>
2022-04-26 01:35:08 +0530

    Updated Headphones Page

--- src/pages/Headphones.jsx
+++ src/pages/Headphones.jsx
@@ -4,6 +4,7 @@ import Card from '../component/Card';
 const productData = [
   {
     path: "/assets/headphone_side.png",
+    id: 100,
     quote: "New Product",
     head: "XX99 Mark II Headphones",
     body: "Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.",
@@ -11,12 +12,14 @@ const productData = [
   },
   {
     path: "/assets/headphone_white2.png",
+    id: 101,
     head: "XX99 Mark I Headphones",
     body: "Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.",
     buttonText: "See Product"
   },
   {
     path: "/assets/headphone_red.png",
+    id: 102,
     head: "X10 Mark Headphones",
     body: "Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.",
     buttonText: "See Product"
@@ -39,6 +42,7 @@ const Headphones = () => {
             cardHead={value.head}
             buttonText={value.buttonText}
             isbutton={true}
+            buttonLink={`/product/headphone/${value.id}`}
             isreverse={index === 1 && true}
           />
         )}
