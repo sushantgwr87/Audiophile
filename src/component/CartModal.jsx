41dbf04f5da265d60badb37011a5fd2321585812
sushantgwr87 <sushantgangwar87@gmail.com>
2022-04-26 10:33:05 +0530

    Added CartModal Component

---
+++ src/component/CartModal.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ReactDOM from "react-dom";
+import useMountTransition from "../customHook/useMountTransition";
+import styles from "../styles/modal.module.css";
+
+const CartModal = ({ show, onClose }) => {
+
+  const [isBrowser, setIsBrowser] = useState(false);
+  const hasTransitionedIn = useMountTransition(show, 1000);
+
+  useEffect(() => {
+    setIsBrowser(true);
+    const closeOnEscapeKeyDown = e => {
+      if ((e.charCode || e.keyCode) === 27) {
+        onClose();
+      }
+    };
+    document.body.addEventListener("keydown", closeOnEscapeKeyDown);
+    return () => document.body.removeEventListener("keydown", closeOnEscapeKeyDown);
+  }, [onClose]);
+
+  const modalContent = hasTransitionedIn || show ? (
+    <div className={`${styles.modal_overlay} ${hasTransitionedIn && show ? styles.modal_show : styles.modal_hide}`} onClick={onClose}>
+      <div className={`${styles.modal} ${hasTransitionedIn && show ? styles.modal_show : styles.modal_hide}`} onClick={e => e.stopPropagation()}>
+          Testing
+      </div>
+    </div>
+  ) : null
+
+  if (isBrowser) {
+    return ReactDOM.createPortal(
+      modalContent,
+      document.getElementById("modal")
+    );
+  } else {
+    return null;
+  }
+};
+
+export default CartModal;
