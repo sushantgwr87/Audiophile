7cdef74bff7ae8cc11f04f145d891ebfd31bf25c
sushantgwr87 <sushantgangwar87@gmail.com>
2022-04-27 06:19:44 +0530

    Added Counter Button Component

---
+++ src/component/CounterButton.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+const CounterButton = ({handleCallback}) => {
+
+    const [count, setCount] = useState(1);
+
+    const handleDecrement = () => {
+        if (count === 0)
+            return;
+        setCount(count - 1);
+        handleCallback(count-1)
+    }
+    const handleIncrement = () => {
+        setCount(count + 1);
+        handleCallback(count+1)
+    }
+
+    return (
+        <div className="counter_button">
+            <button onClick={handleDecrement}>-</button>
+            <span>{count}</span>
+            <button onClick={handleIncrement}>+</button>
+        </div>
+    )
+}
+
+export default CounterButton
