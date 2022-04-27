05baae587b6c3566be5f0ece4c683c2cb95e6109
sushantgwr87 <sushantgangwar87@gmail.com>
2022-04-27 06:19:19 +0530

    Updated Modal Component

--- src/component/CartModal.jsx
+++ src/component/CartModal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import ReactDOM from "react-dom";
 import useMountTransition from "../customHook/useMountTransition";
@@ -6,11 +6,9 @@ import styles from "../styles/modal.module.css";
 
 const CartModal = ({ show, onClose }) => {
 
-  const [isBrowser, setIsBrowser] = useState(false);
   const hasTransitionedIn = useMountTransition(show, 1000);
 
   useEffect(() => {
-    setIsBrowser(true);
     const closeOnEscapeKeyDown = e => {
       if ((e.charCode || e.keyCode) === 27) {
         onClose();
@@ -23,19 +21,26 @@ const CartModal = ({ show, onClose }) => {
   const modalContent = hasTransitionedIn || show ? (
     <div className={`${styles.modal_overlay} ${hasTransitionedIn && show ? styles.modal_show : styles.modal_hide}`} onClick={onClose}>
       <div className={`${styles.modal} ${hasTransitionedIn && show ? styles.modal_show : styles.modal_hide}`} onClick={e => e.stopPropagation()}>
-          Testing
+          <div className={styles.modal_header}>
+            <h3>Cart - <span>2</span></h3>
+            <button>Remove All</button>
+          </div>
+          <div className={styles.modal_body}></div>
+          <div className={styles.modal_footer}>
+            <div className={styles.modal_footer___total_bill}>
+              <h3>Total</h3>
+              <h2>value</h2>
+            </div>
+            <Link to={"/checkout"} onClick={onClose}>Continue to Payout</Link>
+          </div>
       </div>
     </div>
   ) : null
 
-  if (isBrowser) {
     return ReactDOM.createPortal(
       modalContent,
       document.getElementById("modal")
     );
-  } else {
-    return null;
-  }
 };
 
 export default CartModal;
