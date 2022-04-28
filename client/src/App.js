aa1e149b3d34567da68c57640ea4d79157b39cc2
sushantgwr87 <sushantgangwar87@gmail.com>
2022-04-28 21:42:40 +0530

    Added Root App files

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import Navbar from "./component/Navbar";
+import Footer from "./component/Footer";
+import Headphones from "./pages/Headphones";
+import Earphones from "./pages/Earphones";
+import Speakers from "./pages/Speakers";
+import Checkout from "./pages/Checkout";
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ScrollToTop from "./component/ScrollToTop";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <ScrollToTop>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/headphones" element={<Headphones />} />
+            <Route path="/speakers" element={<Speakers />} />
+            <Route path="/earphones" element={<Earphones />} />
+            <Route path="/product/:category/:id" element={<Product />} />
+            <Route path="/checkout" element={<Checkout />} />
+            {/* <Route component={NotFound} /> */}
+          </Routes>
+        </div>
+      </ScrollToTop>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
