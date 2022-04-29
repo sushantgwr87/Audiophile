245b2145554ac79b9152d56b674e8cbdbd614581
sushantgwr87 <sushantgangwar87@gmail.com>
2022-04-29 19:05:35 +0530

    Updated CheckoutCard Component

--- client/src/component/CheckoutCard.jsx
+++ client/src/component/CheckoutCard.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import CounterButton from './CounterButton';
+import styles from '../styles/checkoutCard.module.css'
 
-const CheckoutCard = ({ isModal = false }) => {
+const CheckoutCard = ({ isModal = false, imagePath, productName, productPrice = null, productQuantity }) => {
     return (
-        <div className='checkout_card'>
-            <div className="checkout_product___image">
-                <img src="/assets/headphone_white2.png" alt="Product" />
+        <div className={styles.checkout_card}>
+            <div className={styles.checkout_product___image}>
+                <img src={imagePath} alt="Product" />
             </div>
-            <div className="checkout_product___details">
-                <h3>XII</h3>
-                <p>&#8377; 2,999</p>
+            <div className={styles.checkout_product___details}>
+                <h3>{productName}</h3>
+                <p>&#8377; {productPrice.toLocaleString()}</p>
             </div>
-            <div className="checkout_product___action">
-                {isModal ? <CounterButton /> : <h4>x2</h4>}
+            <div className={styles.checkout_product___action}>
+                {isModal ? <CounterButton /> : <h4>x{productQuantity}</h4>}
             </div>
         </div>
     )
