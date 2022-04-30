b52a378d4f89f90bf4c981ceec95e7c0f0bbd6dd
sushantgwr87 <sushantgangwar87@gmail.com>
2022-05-01 00:12:07 +0530

    Added Product Actions

--- client/src/actions/product.js
+++ client/src/actions/product.js
@@ -1,5 +1,33 @@
-export default async function getProducts() {
-    const response = await fetch(`http://localhost:5000/product/`);
+export async function getCategoryProducts(category) {
+    const response = await fetch(`http://localhost:5000/products/`, { params: { category } });
+
+    if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+    }
+
+    const productData = await response.json();
+
+    console.log(productData);
+}
+
+export async function getProduct(category, id) {
+    const response = await fetch(`http://localhost:5000/products`, { params: { category, id } });
+
+    if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+    }
+
+    const productData = await response.json();
+
+    console.log(productData);
+}
+
+export async function addProduct(category, id) {
+    const response = await fetch(`http://localhost:5000/products`, { params: { category, id } });
 
     if (!response.ok) {
         const message = `An error occurred: ${response.statusText}`;
