c9fb371ffd4cf46e474c725a16172ee7587a70b6
sushantgwr87 <sushantgangwar87@gmail.com>
2022-05-02 06:17:17 +0530

    Updated Server

--- server/server.js
+++ server/server.js
@@ -5,15 +5,21 @@ require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/product"));
+
+app.use(express.urlencoded({extended: true}));
+// app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static('public'));
+
+app.use(require("./routes/products"));
 // get driver connection
 const dbo = require("./db/connector");
- 
+
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
- 
+
   });
   console.log(`Server is running on port: ${port}`);
 });
