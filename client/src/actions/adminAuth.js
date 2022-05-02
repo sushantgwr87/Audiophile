9bb9bbf312a4b7f2392ee70d376f9b4007383ad6
sushantgwr87 <sushantgangwar87@gmail.com>
2022-05-02 06:16:49 +0530

    Updated Actions

--- client/src/actions/adminAuth.js
+++ client/src/actions/adminAuth.js
@@ -1,9 +1,11 @@
-export async function auth({login,user}) {
-    await fetch(`http://localhost:5000/auth`, {
-     method: "POST",
-     body: JSON.stringify(),
-     headers: {
-       'Content-Type': 'application/json'
-     },
-   });
+export async function auth({ user, password }) {
+  const body = JSON.stringify({ user, password });
+  const response = await fetch(`http://localhost:5000/auth`, {
+    method: "POST",
+    body: body,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  });
+  return response.json();
 }
