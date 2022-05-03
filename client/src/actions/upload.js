006fe5a24f54c8750d260f6c8e2b0f93e3df8c92
sushantgwr87 <sushantgangwar87@gmail.com>
2022-05-03 23:45:44 +0530

    Updated Upload Action

--- client/src/actions/upload.js
+++ client/src/actions/upload.js
@@ -10,6 +10,19 @@ export async function imageUpload(image) {
   const res = await domainurl.post("image/upload", formData);
 
   console.log(res.data)
+
+  return res.data;
+}
+
+export async function productUpload(formData) {
+
+  console.log(formData);
+  const body = JSON.stringify(formData)
+  console.log(body);
+  
+  const res = await domainurl.post("product/add",body);
+
+  console.log(res.data)
   // const imageData = res.json();
 
   // return imageData;
