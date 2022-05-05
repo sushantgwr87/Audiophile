cd7561c238eeed812a84b99fb6cb38920d5b15a6
sushantgwr87 <sushantgangwar87@gmail.com>
2022-05-05 14:40:10 +0530

    Updated Server

--- server/server.js
+++ server/server.js
@@ -1,23 +1,35 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 require("dotenv").config({ path: "./config.env" });
+
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.urlencoded({extended: true}));
+// const mongoose = require("mongoose");
+const Router = require("./routes/products")
+app.use(Router);
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require("./routes/products"));
+// app.use(require("./routes/products"));
 // get driver connection
-const dbo = require("./db/connector");
+const db = require("./db/connector");
 
 app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-
+  db.on("error", console.error.bind(console, "connection error: "));
+  db.once("open", function () {
+    console.log("MongoDB Connected successfully");
   });
+  // perform a database connection when server starts
+  // dbo.connectToServer(function (err) {
+  //   if (err) console.error(err);
+
+  // });
   console.log(`Server is running on port: ${port}`);
 });
