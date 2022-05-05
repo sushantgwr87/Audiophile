f69efaadeef27d83ce7287eafa3fdef97f7cc497
sushantgwr87 <sushantgangwar87@gmail.com>
2022-05-05 14:40:31 +0530

    Updated Connector with Mongoose

--- server/db/connector.js
+++ server/db/connector.js
@@ -1,26 +1,41 @@
-const { MongoClient } = require("mongodb");
+// const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
 const Db = process.env.ATLAS_URI;
-const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
- 
-var _db;
- 
-module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("audiophile");
-        console.log("Successfully connected to MongoDB."); 
-      }
-      return callback(err);
-         });
-  },
- 
-  getDb: function () {
-    return _db;
-  },
-};
+// const client = new MongoClient(Db, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+mongoose.connect(Db,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+// var _db;
+
+// module.exports = {
+//   connectToServer: function (callback) {
+//     client.connect(function (err, db) {
+//       // Verify we got a good "db" object
+//       if (db) {
+//         _db = db.db("audiophile");
+//         console.log("Successfully connected to MongoDB.");
+//       }
+//       return callback(err);
+//     });
+//   },
+
+//   getDb: function () {
+//     return _db;
+//   },
+// };
+
+const db = mongoose.connection;
+
+module.exports = db;
+// db.on("error", console.error.bind(console, "connection error: "));
+// db.once("open", function () {
+//   console.log("Connected successfully");
+// });
