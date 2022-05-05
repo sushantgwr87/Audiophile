56ed9dd458a3698d652ee8f349b3438ff6938b77
sushantgwr87 <sushantgangwar87@gmail.com>
2022-05-06 00:11:05 +0530

    Updated Connector

--- server/db/connector.js
+++ server/db/connector.js
@@ -1,10 +1,5 @@
-// const { MongoClient } = require("mongodb");
 const mongoose = require("mongoose");
 const Db = process.env.ATLAS_URI;
-// const client = new MongoClient(Db, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
 
 mongoose.connect(Db,
   {
@@ -13,29 +8,6 @@ mongoose.connect(Db,
   }
 );
 
-// var _db;
-
-// module.exports = {
-//   connectToServer: function (callback) {
-//     client.connect(function (err, db) {
-//       // Verify we got a good "db" object
-//       if (db) {
-//         _db = db.db("audiophile");
-//         console.log("Successfully connected to MongoDB.");
-//       }
-//       return callback(err);
-//     });
-//   },
-
-//   getDb: function () {
-//     return _db;
-//   },
-// };
-
 const db = mongoose.connection;
 
 module.exports = db;
-// db.on("error", console.error.bind(console, "connection error: "));
-// db.once("open", function () {
-//   console.log("Connected successfully");
-// });
