520995acfdb269bd011654120f357551f92b324f
sushantgwr87 <sushantgangwar87@gmail.com>
2022-05-07 00:05:10 +0530

    Updated Headphones Page

--- client/src/pages/Headphones.jsx
+++ client/src/pages/Headphones.jsx
@@ -1,32 +1,17 @@
-import React from 'react';
+import { useEffect } from 'react';
 import Card from '../component/Card';
-
-const productData = [
-  {
-    path: "/assets/headphone_side.png",
-    id: 100,
-    quote: "New Product",
-    head: "XX99 Mark II Headphones",
-    body: "Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.",
-    buttonText: "See Product"
-  },
-  {
-    path: "/assets/headphone_white2.png",
-    id: 101,
-    head: "XX99 Mark I Headphones",
-    body: "Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.",
-    buttonText: "See Product"
-  },
-  {
-    path: "/assets/headphone_red.png",
-    id: 102,
-    head: "X10 Mark Headphones",
-    body: "Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.",
-    buttonText: "See Product"
-  },
-]
+import { getCategoryProducts } from '../actions/product';
 
 const Headphones = () => {
+
+  useEffect(() => {
+    getCategoryProducts("headphones");
+  }, [])
+
+  const productData = JSON.parse(localStorage.getItem("headphonesProducts"));
+
+  // console.log(productData)
+
   return (
     <>
       <div className="page_title">
@@ -35,15 +20,15 @@ const Headphones = () => {
       <div className="page_content">
         {productData.map((value, index) =>
           <Card
-            key={index}
+            key={value._id}
             imagePath={value.path}
             cardQuote={value.quote}
-            cardBody={value.body}
-            cardHead={value.head}
-            buttonText={value.buttonText}
+            cardBody={value.caption}
+            cardHead={value.longTitle}
+            buttonText={"See Product"}
             isbutton={true}
-            buttonLink={`/product/headphone/${value.id}`}
-            isreverse={index === 1 && true}
+            buttonLink={`/product/${value._id}`}
+            isreverse={index % 2 !== 0 && true}
           />
         )}
       </div>
