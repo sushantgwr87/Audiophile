14861aa6b22a3accdbbeac79804f5cdea4c7b837
sushantgwr87 <sushantgangwar87@gmail.com>
2022-05-08 06:50:25 +0530

    Updated Speaker Page

--- client/src/pages/Speakers.jsx
+++ client/src/pages/Speakers.jsx
@@ -1,37 +1,45 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Card from '../component/Card';
 import { getCategoryProducts } from '../actions/product';
-
+import Loader from '../component/Loader';
+import useLocalStorage from '../customHook/useLocalStorage';
 const Speakers = () => {
 
+  const [isLoading, setIsLoading] = useState(true)
+  const [productData, setproductData] = useLocalStorage("speakerProduct", null)
+
   useEffect(() => {
-    getCategoryProducts("speaker");
-  })
+    const getData = async () => {
+      const data = await getCategoryProducts("speaker");
+      setproductData(data);
+      setIsLoading(false)
+    }
+    getData();
+  }, [])
 
-  const productData = JSON.parse(sessionStorage.getItem("speakerProducts"));
-
-  console.log(productData)
 
   return (
     <>
       <div className="page_title">
         Speakers
       </div>
-      <div className="page_content">
-        {productData.map((value, index) =>
-          <Card
-            key={value._id}
-            imagePath={value.path}
-            cardQuote={value.quote}
-            cardBody={value.caption}
-            cardHead={value.longTitle}
-            buttonText={"See Product"}
-            isbutton={true}
-            buttonLink={`/product/${value._id}`}
-            isreverse={index % 2 !== 0 && true}
-          />
-        )}
-      </div>
+      {isLoading ? <Loader /> :
+        <div className="page_content">
+          {productData.map((value, index) =>
+            <Card
+              key={value._id}
+              imagePath={value.path}
+              cardQuote={value.quote}
+              cardBody={value.caption}
+              cardHead={value.longTitle}
+              buttonText={"See Product"}
+              isbutton={true}
+              buttonLink={`/product/${value._id}`}
+              isreverse={index % 2 !== 0 && true}
+            />
+          )}
+        </div>
+      }
     </>
   )
 }
