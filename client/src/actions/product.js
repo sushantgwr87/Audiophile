5e92fe0000b150ff47b5c43741e6cc94b1157a5a
sushantgwr87 <sushantgangwar87@gmail.com>
2022-05-08 06:49:12 +0530

    Updated Product Actions

--- client/src/actions/product.js
+++ client/src/actions/product.js
@@ -3,9 +3,8 @@ import domainurl from "../domainAPI";
 export async function getCategoryProducts(category) {
     try {
         const res = await domainurl.get(`${category}/products`);
-        console.log(res.data)
         if (res.data.status)
-            sessionStorage.setItem(`${category}Products`, JSON.stringify(res.data.data));
+            return res.data.data;
     }
     catch (error) {
         console.log(error);
@@ -16,8 +15,8 @@ export async function getCategoryProducts(category) {
 export async function getFeaturedProducts() {
     try {
         const res = await domainurl.get(`/featured`);
-        console.log(res.data)
-        sessionStorage.setItem("featuredProducts", JSON.stringify(res.data.data));
+        if (res.data.status)
+            return res.data.data;
     }
     catch (error) {
         console.log(error);
@@ -25,10 +24,12 @@ export async function getFeaturedProducts() {
 }
 
 export async function getProduct(id) {
-    const res = await domainurl.get(`product/${id}`);
-
-    sessionStorage.setItem("fetchedProduct", JSON.stringify(res.data.data));
-    const productData = res.data;
-
-    console.log(productData);
+    try {
+        const res = await domainurl.get(`product/${id}`);
+        if (res.data.status)
+            return res.data.data;
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
