9205bba94fe16ef01941d17ec5e0c85fdaf3ceb2
sushantgwr87 <sushantgangwar87@gmail.com>
2022-05-09 18:34:56 +0530

    Updated Product Actions

--- client/src/actions/product.js
+++ client/src/actions/product.js
@@ -1,13 +1,17 @@
 import domainurl from "../domainAPI";
+import { toast } from "react-toastify";
 
 export async function getCategoryProducts(category) {
     try {
         const res = await domainurl.get(`${category}/products`);
-        if (res.data.status)
-            return res.data.data;
+        if (res.data.status) {
+            return { error: false, data: res.data.data };
+        }
     }
     catch (error) {
         console.log(error);
+        toast.error("Data fetch failed, Please refresh or try again later");
+        return { error: true }
     }
 
 }
@@ -15,21 +19,27 @@ export async function getCategoryProducts(category) {
 export async function getFeaturedProducts() {
     try {
         const res = await domainurl.get(`/featured`);
-        if (res.data.status)
-            return res.data.data;
+        if (res.data.status) {
+            return { error: false, data: res.data.data };
+        }
     }
     catch (error) {
         console.log(error);
+        toast.error("Data fetch failed, Please refresh or try again later");
+        return { error: true }
     }
 }
 
 export async function getProduct(id) {
     try {
         const res = await domainurl.get(`product/${id}`);
-        if (res.data.status)
-            return res.data.data;
+        if (res.data.status) {
+            return { error: false, data: res.data.data };
+        }
     }
     catch (error) {
         console.log(error);
+        toast.error("Data fetch failed, Please refresh or try again later");
+        return { error: true }
     }
 }
