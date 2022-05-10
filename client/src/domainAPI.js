c853399d24fa85fb9f8b262ef94f6899e3805ac6
sushantgwr87 <sushantgangwar87@gmail.com>
2022-05-10 15:17:32 +0530

    Updated API Url

--- client/src/domainAPI.js
+++ client/src/domainAPI.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+const dev = process.env.NODE_ENV !== 'production';
+
+let { REACT_APP_PUBLIC_API_URL, REACT_APP_PRODUCTION_API_URL } = process.env;
+
+const apiUrl = dev ? REACT_APP_PUBLIC_API_URL : REACT_APP_PRODUCTION_API_URL;
+
 const domainurl = axios.create({
-  baseURL: 'http://localhost:5000/',
+  baseURL: apiUrl,
   headers: {
     "Content-Type": "application/json",
   },
